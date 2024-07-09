676349916e3f88e1a8e5b73cab952e3fefc85aad
vignesh <vigneshten5@gmail.com>
2024-07-10 02:23:06 +0530

    20

--- src/app/terms/page.js
+++ src/app/terms/page.js
@@ -1,19 +1,33 @@
-"use client"
 import React from "react";
 import { motion } from "framer-motion";
 import Footer from "@/components/Footer/Footer";
 
 const TermsConditions = () => {
   return (
-    <div className="bg-#101010">
-    <div className="relative flex flex-col justify-center m-[140px] items-center">
-      <h1 className="text-4xl font-bold mb-6">
-        Website Terms and Conditions of Use
-      </h1>
-      <div className="space-y-4">
+    <div className="bg-black text-white min-h-screen">
+      <div className="relative flex flex-col justify-center mx-auto max-w-3xl p-8 space-y-4">
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          className="text-4xl font-bold mb-6"
+        >
+          Website Terms and Conditions of Use
+        </motion.h1>
         <section>
-          <h2 className="text-2xl font-semibold mb-2">1. Terms</h2>
-          <p>
+          <motion.h2
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="text-2xl font-semibold mb-2"
+          >
+            1. Terms
+          </motion.h2>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
             By accessing this Website, accessible from{" "}
             <a
               href="https://subadha.netlify.app/"
@@ -28,121 +42,237 @@ const TermsConditions = () => {
             applicable local laws. If you disagree with any of these terms, you
             are prohibited from accessing this site. The materials contained in
             this Website are protected by copyright and trademark law.
-          </p>
+          </motion.p>
         </section>
 
         <section>
-          <h2 className="text-2xl font-semibold mb-2">2. Use License</h2>
-          <p>
+          <motion.h2
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="text-2xl font-semibold mb-2"
+          >
+            2. Use License
+          </motion.h2>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
             Permission is granted to temporarily download one copy of the
-            materials on Subadha's Website for personal, non-commercial
+            materials on Subadha&apos;s Website for personal, non-commercial
             transitory viewing only. This is the grant of a license, not a
             transfer of title, and under this license you may not:
-          </p>
-          <ul className="list-disc list-inside ml-4">
-            <li>modify or copy the materials;</li>
-            <li>
+          </motion.p>
+          <motion.ul
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="list-disc list-inside ml-4"
+          >
+            <motion.li
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            >
+              modify or copy the materials;
+            </motion.li>
+            <motion.li
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            >
               use the materials for any commercial purpose or for any public
               display;
-            </li>
-            <li>
-              attempt to reverse engineer any software contained on Subadha's
-              Website;
-            </li>
-            <li>
+            </motion.li>
+            <motion.li
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            >
+              attempt to reverse engineer any software contained on
+              Subadha&apos;s Website;
+            </motion.li>
+            <motion.li
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            >
               remove any copyright or other proprietary notations from the
               materials; or
-            </li>
-            <li>
+            </motion.li>
+            <motion.li
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            >
               transferring the materials to another person or "mirror" the
               materials on any other server.
-            </li>
-          </ul>
-          <p>
+            </motion.li>
+          </motion.ul>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
             This will let Subadha to terminate upon violations of any of these
             restrictions. Upon termination, your viewing right will also be
             terminated and you should destroy any downloaded materials in your
             possession whether it is printed or electronic format. These Terms
             of Service has been created with the help of the Terms Of Service
             Generator.
-          </p>
+          </motion.p>
         </section>
 
         <section>
-          <h2 className="text-2xl font-semibold mb-2">3. Disclaimer</h2>
-          <p>
-            All the materials on Subadha's Website are provided "as is". Subadha
-            makes no warranties, may it be expressed or implied, therefore
-            negates all other warranties. Furthermore, Subadha does not make any
-            representations concerning the accuracy or reliability of the use of
-            the materials on its Website or otherwise relating to such materials
-            or any sites linked to this Website.
-          </p>
+          <motion.h2
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="text-2xl font-semibold mb-2"
+          >
+            3. Disclaimer
+          </motion.h2>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            All the materials on Subadha&apos;s Website are provided "as is".
+            Subadha makes no warranties, may it be expressed or implied,
+            therefore negates all other warranties. Furthermore, Subadha does
+            not make any representations concerning the accuracy or reliability
+            of the use of the materials on its Website or otherwise relating to
+            such materials or any sites linked to this Website.
+          </motion.p>
         </section>
 
         <section>
-          <h2 className="text-2xl font-semibold mb-2">4. Limitations</h2>
-          <p>
+          <motion.h2
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="text-2xl font-semibold mb-2"
+          >
+            4. Limitations
+          </motion.h2>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
             Subadha or its suppliers will not be hold accountable for any
             damages that will arise with the use or inability to use the
-            materials on Subadha's Website, even if Subadha or an authorize
+            materials on Subadha&apos;s Website, even if Subadha or an authorize
             representative of this Website has been notified, orally or written,
             of the possibility of such damage. Some jurisdiction does not allow
             limitations on implied warranties or limitations of liability for
             incidental damages, these limitations may not apply to you.
-          </p>
+          </motion.p>
         </section>
 
         <section>
-          <h2 className="text-2xl font-semibold mb-2">
+          <motion.h2
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="text-2xl font-semibold mb-2"
+          >
             5. Revisions and Errata
-          </h2>
-          <p>
-            The materials appearing on Subadha's Website may include technical,
-            typographical, or photographic errors. Subadha will not promise that
-            any of the materials in this Website are accurate, complete, or
-            current. Subadha may change the materials contained on its Website
-            at any time without notice. Subadha does not make any commitment to
-            update the materials.
-          </p>
+          </motion.h2>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            The materials appearing on Subadha&apos;s Website may include
+            technical, typographical, or photographic errors. Subadha will not
+            promise that any of the materials in this Website are accurate,
+            complete, or current. Subadha may change the materials contained on
+            its Website at any time without notice. Subadha does not make any
+            commitment to update the materials.
+          </motion.p>
         </section>
 
         <section>
-          <h2 className="text-2xl font-semibold mb-2">6. Links</h2>
-          <p>
+          <motion.h2
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="text-2xl font-semibold mb-2"
+          >
+            6. Links
+          </motion.h2>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
             Subadha has not reviewed all of the sites linked to its Website and
             is not responsible for the contents of any such linked site. The
             presence of any link does not imply endorsement by Subadha of the
-            site. The use of any linked website is at the user's own risk.
-          </p>
+            site. The use of any linked website is at the user&apos;s own risk.
+          </motion.p>
         </section>
 
         <section>
-          <h2 className="text-2xl font-semibold mb-2">
+          <motion.h2
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="text-2xl font-semibold mb-2"
+          >
             7. Site Terms of Use Modifications
-          </h2>
-          <p>
+          </motion.h2>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
             Subadha may revise these Terms of Use for its Website at any time
             without prior notice. By using this Website, you are agreeing to be
             bound by the current version of these Terms and Conditions of Use.
-          </p>
+          </motion.p>
         </section>
 
         <section>
-          <h2 className="text-2xl font-semibold mb-2">8. Your Privacy</h2>
-          <p>Please read our Privacy Policy.</p>
+          <motion.h2
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="text-2xl font-semibold mb-2"
+          >
+            8. Your Privacy
+          </motion.h2>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            Please read our Privacy Policy.
+          </motion.p>
         </section>
 
         <section>
-          <h2 className="text-2xl font-semibold mb-2">9. Governing Law</h2>
-          <p>
-            Any claim related to Subadha's Website shall be governed by the laws
-            of without regards to its conflict of law provisions.
-          </p>
+          <motion.h2
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className="text-2xl font-semibold mb-2"
+          >
+            9. Governing Law
+          </motion.h2>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            Any claim related to Subadha&apos;s Website shall be governed by the
+            laws without regards to its conflict of law provisions.
+          </motion.p>
         </section>
       </div>
-      
-    </div>
+      <Footer />
     </div>
   );
 };
