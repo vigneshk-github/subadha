b9f4e8572346140d20a6cda81aa1d661d68ae675
vignesh <vigneshten5@gmail.com>
2024-07-16 15:13:29 +0530

    64

--- src/app/Terms/page.js
+++ src/app/Terms/page.js
@@ -27,7 +27,7 @@ export default function TermsAndConditions() {
           <h2 className="text-xl font-semibold">2. Use License</h2>
           <p>
             Permission is granted to temporarily download one copy of the
-            materials on Subadha&apos;s Website for personal, non-commercial
+            materials on Subadhas Website for personal, non-commercial
             transitory viewing only. This is the grant of a license, not a
             transfer of title, and under this license you may not:
           </p>
@@ -38,8 +38,8 @@ export default function TermsAndConditions() {
               display;
             </li>
             <li>
-              attempt to reverse engineer any software contained on
-              Subadha&apos;s Website;
+              attempt to reverse engineer any software contained on Subadhas
+              Website;
             </li>
             <li>
               remove any copyright or other proprietary notations from the
@@ -61,34 +61,33 @@ export default function TermsAndConditions() {
 
           <h2 className="text-xl font-semibold">3. Disclaimer</h2>
           <p>
-            All the materials on Subadha&apos;s Website are provided as is.
-            Subadha makes no warranties, may it be expressed or implied,
-            therefore negates all other warranties. Furthermore, Subadha does
-            not make any representations concerning the accuracy or reliability
-            of the use of the materials on its Website or otherwise relating to
-            such materials or any sites linked to this Website.
+            All the materials on Subadhas Website are provided as is. Subadha
+            makes no warranties, may it be expressed or implied, therefore
+            negates all other warranties. Furthermore, Subadha does not make any
+            representations concerning the accuracy or reliability of the use of
+            the materials on its Website or otherwise relating to such materials
+            or any sites linked to this Website.
           </p>
 
           <h2 className="text-xl font-semibold">4. Limitations</h2>
           <p>
             Subadha or its suppliers will not be held accountable for any
             damages that will arise with the use or inability to use the
-            materials on Subadha&apos;s Website, even if Subadha or an
-            authorized representative of this Website has been notified, orally
-            or written, of the possibility of such damage. Some jurisdictions do
-            not allow limitations on implied warranties or limitations of
-            liability for incidental damages, these limitations may not apply to
-            you.
+            materials on Subadhas Website, even if Subadha or an authorized
+            representative of this Website has been notified, orally or written,
+            of the possibility of such damage. Some jurisdictions do not allow
+            limitations on implied warranties or limitations of liability for
+            incidental damages, these limitations may not apply to you.
           </p>
 
           <h2 className="text-xl font-semibold">5. Revisions and Errata</h2>
           <p>
-            The materials appearing on Subadha&apos;s Website may include
-            technical, typographical, or photographic errors. Subadha will not
-            promise that any of the materials in this Website are accurate,
-            complete, or current. Subadha may change the materials contained on
-            its Website at any time without notice. Subadha does not make any
-            commitment to update the materials.
+            The materials appearing on Subadhas Website may include technical,
+            typographical, or photographic errors. Subadha will not promise that
+            any of the materials in this Website are accurate, complete, or
+            current. Subadha may change the materials contained on its Website
+            at any time without notice. Subadha does not make any commitment to
+            update the materials.
           </p>
 
           <h2 className="text-xl font-semibold">6. Links</h2>
@@ -96,7 +95,7 @@ export default function TermsAndConditions() {
             Subadha has not reviewed all of the sites linked to its Website and
             is not responsible for the contents of any such linked site. The
             presence of any link does not imply endorsement by Subadha of the
-            site. The use of any linked website is at the user&apos;s own risk.
+            site. The use of any linked website is at the users own risk.
           </p>
 
           <h2 className="text-xl font-semibold">
@@ -111,7 +110,10 @@ export default function TermsAndConditions() {
           <h2 className="text-xl font-semibold">8. Your Privacy</h2>
           <p>
             Please read our{" "}
-            <a href="/Policy" className="text-blue-600 dark:text-blue-400">
+            <a
+              href="/Policy"
+              className="text-blue-600 dark:text-blue-400"
+            >
               Privacy Policy
             </a>
             .
@@ -119,8 +121,8 @@ export default function TermsAndConditions() {
 
           <h2 className="text-xl font-semibold">9. Governing Law</h2>
           <p>
-            Any claim related to Subadha&apos;s Website shall be governed by the
-            laws of in without regards to its conflict of law provisions.
+            Any claim related to Subadhas Website shall be governed by the laws
+            of in without regards to its conflict of law provisions.
           </p>
         </div>
       </div>
