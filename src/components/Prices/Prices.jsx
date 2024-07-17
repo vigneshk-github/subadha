f1c940eb7b1834a72152b9e13f01829a78f2182b
vignesh <vigneshten5@gmail.com>
2024-07-17 19:54:55 +0530

    77

--- src/components/Prices/Prices.jsx
+++ src/components/Prices/Prices.jsx
@@ -47,12 +47,17 @@ export default function Prices() {
               </div>
               <a
                 href="https://calendly.com/subadha-co-in/subadha"
-                className="rounded-md px-3.5 py-2 mb-5 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-white text-white"
+                className=" ml-5 mb-4  relative inline-block text-sm group"
               >
-                <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-white top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
-                <span className="relative text-white text-xl transition duration-300 group-hover:text-black ease">
-                  Book a Call
+                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                  <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                  <span className="absolute left-0 w-[300px] h-48  transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                  <span className="relative">Book a Call</span>
                 </span>
+                <span
+                  className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                  data-rounded="rounded-lg"
+                ></span>
               </a>
               <ul role="list" className="mb-8 space-y-4 text-left">
                 <li className="flex items-center space-x-3">
@@ -129,12 +134,17 @@ export default function Prices() {
               </div>
               <a
                 href="https://calendly.com/subadha-co-in/subadha"
-                className="rounded-md px-3.5 py-2 mb-5 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-white text-white"
+                className=" ml-5 mb-4  relative inline-block text-sm group"
               >
-                <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-white top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
-                <span className="relative text-white text-xl transition duration-300 group-hover:text-black ease">
-                  Book a Call
+                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                  <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                  <span className="absolute left-0 w-[300px] h-48  transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                  <span className="relative">Book a Call</span>
                 </span>
+                <span
+                  className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                  data-rounded="rounded-lg"
+                ></span>
               </a>
               <ul role="list" className="mb-8 space-y-4 text-left">
                 <li className="flex items-center space-x-3">
@@ -231,12 +241,17 @@ export default function Prices() {
               </div>
               <a
                 href="https://calendly.com/subadha-co-in/subadha"
-                className="rounded-md px-3.5 py-2 mb-5 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-white text-white w-43"
+                className=" ml-5 mb-4 relative inline-block text-sm group"
               >
-                <span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-white top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
-                <span className="relative text-white text-xl transition duration-300 group-hover:text-black ease">
-                  Book a Call
+                <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                  <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                  <span className="absolute left-0 w-[300px] h-48  transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                  <span className="relative">Book a Call</span>
                 </span>
+                <span
+                  className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                  data-rounded="rounded-lg"
+                ></span>
               </a>
               <ul role="list" className="mb-8 space-y-4 text-left">
                 <li className="flex items-center space-x-3">
